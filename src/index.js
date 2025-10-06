576a52fccd137bbcfc4a73ab3ab68d81aea6cf30
Yonexdu0ra <phamngocquy.7303@gmail.com>
2025-10-06 10:20:59 +0700

    thêm log kết nối với csdl

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 // const authMiddleware = require("./middleware/authMiddleware");
 
+const { sequelize } = require("./models");
+
 const PORT = process.env.PORT || 3002;
 
 
@@ -26,6 +28,17 @@ app.get("/", (req, res) => {
     return res.send("Hello World");
 });
 
+(async () => {
+    try {
+        await sequelize.authenticate();
+
+        // await sequelize.sync({ force: true });
+
+        console.log("Kết nối đến database thành công");
+    } catch (error) {
+        console.error("Không thể kết nối đến database:", error.message);
+    }
+})();
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
