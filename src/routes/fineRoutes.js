6d40262d4940c083cf9d826db3cba2da7ce2b67b
yonexdu0ra <phamngocquy.7303@gmail.com>
2025-10-18 09:35:16 +0700

    thêm view add fines

--- src/routes/fineRoutes.js
+++ src/routes/fineRoutes.js
@@ -3,5 +3,6 @@ const fineController = require('../controllers/fineController');
 const router = Router();
 
 router.get('/', fineController.renderViewFines);
+router.get('/add', fineController.renderViewCreateFine);
 
 module.exports = router;
