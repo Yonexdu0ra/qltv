8b846d71e69b9c6b9c5c781f89a148aae6cf4495
Yonexdu0ra <phamngocquy.7303@gmail.com>
2025-10-21 21:08:32 +0700

    feat: Add route for viewing fine details with librarian and admin access

--- src/routes/private/fineRoutes.js
+++ src/routes/private/fineRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get('/reader', fineController.renderViewFineForReader);
 router.get('/', requiredRoleLibrarianAndAdmin, fineController.renderViewFines);
+router.get('/detail/:id', requiredRoleLibrarianAndAdmin, fineController.renderViewFineDetail);
 router.get('/add', requiredRoleLibrarianAndAdmin, fineController.renderViewCreateFine);
 router.post('/add', requiredRoleLibrarianAndAdmin, fineController.handlerCreateFine);
 router.get('/mark-as-paid/:id', requiredRoleLibrarianAndAdmin, fineController.handlerMarkAsPaidFine);
