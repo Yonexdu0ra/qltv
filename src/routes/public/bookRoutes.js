9a3055d5b312021223ee21adc8d5239a73ed74a1
Yonexdu0ra <phamngocquy.7303@gmail.com>
2025-10-21 09:32:18 +0700

    feat: Update routes for authors, books, and genres to improve readability and consistency; adjust book detail links in borrow view

--- src/routes/public/bookRoutes.js
+++ src/routes/public/bookRoutes.js
@@ -3,6 +3,7 @@ const BookController = require('../../controllers/bookController');
 const router = Router();
 
 
+router.get('/', BookController.renderViewBooksForReader);
 router.get('/search', BookController.handleSearchBooks);
 router.get('/:slug', BookController.renderViewDetailBook);
 
--- src/routes/public/genreRoutes.js
+++ src/routes/public/genreRoutes.js
@@ -2,8 +2,8 @@ const { Router} = require("express");
 const authController = require("../../controllers/genreController");
 const router = Router();
 
-
-router.get("/search", authController.handleSearchGenre)
+router.get("/", authController.renderViewGenresForReader);
+router.get("/search", authController.handleSearchGenre);
 router.get("/:slug", authController.renderViewDetailGenre);
 
 module.exports = router;
